c11e7bdd98d83a6cd3a26813fa8f9ee0c9c08d5c
Alex <106420816+Alexcrdv@users.noreply.github.com>
2023-11-16 22:19:18 -0600

    Added functionality to calculator

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,60 @@
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const operators = ["/", "x", "-", "+"]
+let input = "";
+let output = "";
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculation(e.target.dataset.value))
+})
+
+const calculation = (btnValue) => {
+    if (numbers.includes(btnValue)) {
+        input += btnValue;
+    }
+
+    if (btnValue === "." && !input.includes(".")) {
+        input += btnValue;
+    }
+
+    if (btnValue === "+/-" && input !== "") {
+        input = input * -1;
+    }
+
+    if (btnValue === "C") {
+        input = "";
+        firstNum = "";
+        secondNum = "";
+        operator = "";
+    }
+
+    if (btnValue === "CE") {
+        input = input.slice(0, -1);
+        console.log(input);
+    }
+
+    if (input !== "" && operators.includes(btnValue)) {
+        firstNum = input;
+        input = "";
+        operator = btnValue;
+    }
+
+    if (btnValue === "=" && firstNum !== "" && operator !== "") {
+        secondNum = input;
+        input = operate(firstNum, secondNum, operator);
+        firstNum = "";
+        secondNum = "";
+    }
+
+    display.textContent = input; 
+}
+
 function sum (a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
