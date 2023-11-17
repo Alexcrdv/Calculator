05c74a3f89345df6c07a6905e4921bed8f4ac0ba
Alex <106420816+Alexcrdv@users.noreply.github.com>
2023-11-17 11:50:10 -0600

    Added additional functionality to calculator buttons and created another display that showcases full operation.  Calculator is currently fully functional

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,15 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
+const smallDisplay = document.querySelector('.small-display');
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const operators = ["/", "x", "-", "+"]
 let input = "";
+let smallInput = "";
 let output = "";
 let firstNum = "";
 let operator = "";
 let secondNum = "";
+let needsClear = false;
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => calculation(e.target.dataset.value))
@@ -14,15 +17,31 @@ buttons.forEach((button) => {
 
 const calculation = (btnValue) => {
     if (numbers.includes(btnValue)) {
+        if (needsClear && input !== "") {
+            input = "";
+            firstNum = "";
+            secondNum = "";
+            operator = "";
+            smallInput = "";
+            needsClear = false;
+        }
         input += btnValue;
+        smallInput += btnValue;
     }
 
     if (btnValue === "." && !input.includes(".")) {
         input += btnValue;
+        smallInput += btnValue;
     }
 
     if (btnValue === "+/-" && input !== "") {
-        input = input * -1;
+        if (operator !== "") {
+            smallInput = smallInput.substring(0, smallInput.indexOf(operator)) + smallInput.substring(smallInput.indexOf(operator)).replace(input, input * -1);
+            input = input * -1;
+        } else {
+            input = input * -1;
+            smallInput = input;
+        }
     }
 
     if (btnValue === "C") {
@@ -30,27 +49,63 @@ const calculation = (btnValue) => {
         firstNum = "";
         secondNum = "";
         operator = "";
+        smallInput = "";
+        needsClear = false;
     }
 
     if (btnValue === "CE") {
-        input = input.slice(0, -1);
-        console.log(input);
+        if (smallInput.toString().includes("=")) {
+            input = "";
+            firstNum = "";
+            secondNum = "";
+            operator = "";
+            smallInput = "";
+            needsClear = false;
+        }
+
+        if (input !== "" && operator !== "" && firstNum !== "") {
+            input = "";
+            smallInput = firstNum + operator;
+        } else if (input === "" && operator !== "" && firstNum !== "") {
+            operator = "";
+            smallInput = firstNum;
+            input = firstNum;
+        } else {
+            input = "";
+            smallInput = "";
+        }
     }
 
     if (input !== "" && operators.includes(btnValue)) {
-        firstNum = input;
-        input = "";
+        if (operator !== "") {
+            secondNum = input;
+            input = operate(firstNum, secondNum, operator);
+            firstNum = "";
+            secondNum  = "";
+        }
+
+        if (smallInput.toString().includes("=")) {
+            smallInput = input;
+        }
+
         operator = btnValue;
+        firstNum = input;
+        smallInput += operator;
+        input = "";
+        needsClear = false;
     }
 
     if (btnValue === "=" && firstNum !== "" && operator !== "") {
         secondNum = input;
         input = operate(firstNum, secondNum, operator);
-        firstNum = "";
         secondNum = "";
+        operator = "";
+        needsClear = true;
+        smallInput += "=" + input;
     }
 
     display.textContent = input; 
+    smallDisplay.textContent = smallInput;
 }
 
 function sum (a, b) {
